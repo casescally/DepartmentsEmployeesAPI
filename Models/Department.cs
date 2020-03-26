926f28989592264f889e73a544ff85717e06d983
Case Scally <casescally@gmail.com>
2020-03-26 15:30:52 -0500

    first commit

---
+++ Models/Department.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepartmentsEmployeesAPI.Models
+{
+    // C# representation of the Department table
+    public class Department
+    {
+        public int Id { get; set; }
+        public string DeptName { get; set; }
+    }
+}
